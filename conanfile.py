cdbf935a7beac3b869c0b31f792fa22d2f010afb
Terje Loe <terjeloe@hotmail.com>
2021-01-31 14:55:58 +0100

    windows ci working! :)

--- conanfile.py
+++ conanfile.py
@@ -10,28 +10,18 @@ class TEngine(ConanFile):
     url = "<!-- <Package recipe repository url here, for issues about the package> -->"
     description = "Game engine"
     settings = "os", "compiler", "build_type", "arch"
-    requires = "assimp/5.0.1","enet/1.3.17","libpng/1.6.37","glew/2.1.0","luajit/2.0.5"
+    # "assimp/5.0.1" removed for now for shorter ci runtime
+    requires = "enet/1.3.17","libpng/1.6.37","glew/2.1.0","luajit/2.0.5"
     options = {"shared": [True, False]}
     default_options = {"shared": False}
-
-    def _config_cmake(self):
-        if os_info.is_windows:
-            cmake = CMake(self)
-            cmake.configure()
-        else:
-            cmake = CMake(self)
-            cmake.configure()
-        return cmake
 
     def generate(self):
         tc = CMakeToolchain(self)
         tc.generate()
         deps = CMakeDeps(self)
         deps.generate()
-        
+
     def build(self):
-        cmake = self._config_cmake()
-        if os_info.is_windows:
-            cmake.build()
-        else:
-            cmake.build()
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
